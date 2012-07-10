8572d9c60be8bdfa51f9f44ab1757e986d506df2
bamanzi <none@none>
2012-07-10 22:47:33 +0800

    add projectile

---
+++ eepy-project.el
@@ -0,0 +1,21 @@
+;;; eepy-project.el --- project support of EEPY suite
+
+;; This file is part of Enhaneced Emacs for PYthon suite
+;;   http://github.com/bamanzi/eepy
+
+;; Copyright (C) 2012 Ba Manzi <bamanzi@gmail.com>
+;; This file is distributed under GPL v2.
+
+
+
+;;** ropemacs project
+(require 'eepy-ropemacs)
+
+
+;;** eproject
+
+
+;;** projectile
+
+
+(provide 'eepy-project)
