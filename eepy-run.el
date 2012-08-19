7fcb699ae0ef242586d32c56155a43cf870a0003
bamanzi <none@none>
2012-08-19 21:58:29 +0800

    add `python--add-debug-highlight' and `python-insert-breakpoint', stolen from http://www.masteringemacs.org/articles/2012/05/29/compiling-running-scripts-emacs/

--- eepy-run.el
+++ eepy-run.el
@@ -14,6 +14,27 @@
   "Activate a Virtual Environment present using virtualenvwrapper" t)
 
 
+;;** working with pdb
+;; stolen from http://www.masteringemacs.org/articles/2012/05/29/compiling-running-scripts-emacs/
+(defun python--add-debug-highlight ()
+  "Adds a highlighter for use by `python--pdb-breakpoint-string'"
+  (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
+ 
+(add-hook 'python-mode-hook 'python--add-debug-highlight)
+ 
+(defvar python--pdb-breakpoint-string "import pdb; pdb.set_trace() ## DEBUG ##"
+  "Python breakpoint string used by `python-insert-breakpoint'")
+ 
+(defun python-insert-breakpoint ()
+  "Inserts a python breakpoint using `pdb'"
+  (interactive)
+  (back-to-indentation)
+  ;; this preserves the correct indentation in case the line above
+  ;; point is a nested block
+  (split-line)
+  (insert python--pdb-breakpoint-string))
+(define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint)
+
 ;;** highlight error line in compilation result / shell-mode
 ;; stolen from http://www.loveshack.ukfsn.org/emacs/python.el
 (defconst python-compilation-regexp-alist
